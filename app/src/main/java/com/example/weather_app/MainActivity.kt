36e18910bf4245e46fb1f3f685bbf3cb604a5516
muthiikm <muthiikm@gmail.com>
2024-06-10 16:59:08 +0300

    Dispatchers

--- app/src/main/java/com/example/weather_app/MainActivity.kt
+++ app/src/main/java/com/example/weather_app/MainActivity.kt
@@ -1,7 +1,16 @@
 package com.example.weather_app
 
+import android.net.http.HttpException
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings.Global
+import android.widget.Toast
+import androidx.annotation.RequiresExtension
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +20,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        getCurrentWeather()
+
+    }
+
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    private fun getCurrentWeather(){
+        GlobalScope.launch(Dispatchers.IO) {
+            val response = try {
+
+            }catch (e: IOException){
+                Toast.makeText(applicationContext, "app error ${e.message}",Toast.LENGTH_SHORT).show()
+
+
+            }catch (e: HttpException){
+                Toast.makeText(applicationContext, "Http error${e.message}", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+
 
     }
 }
