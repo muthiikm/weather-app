2f5fb7b2f13c2df540414bfee0fa0a5efe875661
muthiikm <muthiikm@gmail.com>
2024-06-11 09:57:54 +0300

    Introduced retrofit to fetch weather data

--- app/src/main/java/com/example/weather_app/MainActivity.kt
+++ app/src/main/java/com/example/weather_app/MainActivity.kt
@@ -7,15 +7,21 @@ import android.provider.Settings.Global
 import android.widget.Toast
 import androidx.annotation.RequiresExtension
 import androidx.appcompat.app.AppCompatActivity
+import com.example.weather_app.databinding.ActivityMainBinding
+import com.example.weather_app.utils.RetrofitInstance
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+//    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -25,19 +31,34 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    @OptIn(DelicateCoroutinesApi::class)
     @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     private fun getCurrentWeather(){
         GlobalScope.launch(Dispatchers.IO) {
             val response = try {
+                RetrofitInstance.api.getCurrentWeather("new york", "metric","")
 
             }catch (e: IOException){
-                Toast.makeText(applicationContext, "app error ${e.message}",Toast.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext, "app error ${e.message}",Toast.LENGTH_SHORT)
+                    .show()
+                return@launch
 
 
             }catch (e: HttpException){
-                Toast.makeText(applicationContext, "Http error${e.message}", Toast.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext, "Http error${e.message}", Toast.LENGTH_SHORT)
+                    .show()
+                return@launch
 
             }
+
+            if (response.isSuccessful && response.body()!= null){
+
+                withContext(Dispatchers.Main){
+
+                    binding.tvTemp.text =" tem: ${response.body()!!.main.temp}"
+                }
+            }
+
         }
 
 
